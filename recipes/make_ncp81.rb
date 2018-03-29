6368b29e5e046c8f2f0ffb82a33178fa23d3aaf0
Ed Overton <e.overton@gmail.com>
2018-03-29 14:37:38 -0400

    add nckl

--- recipes/make_ncp81.rb
+++ recipes/make_ncp81.rb
@@ -10,5 +10,7 @@ include_recipe 'objsrv::install_nco81'
 include_recipe 'objsrv::add_x11'
 # Update Netcool to latest fix pack
 include_recipe 'objsrv::update_nco81'
+# install the netcool knowledge library
+include_recipe 'objsrv::install_nckl'
 # install the netcool probes
 include_recipe '::install_ncp81'
