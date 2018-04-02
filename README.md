b9012d8d651da9018ede5f05ad9c4a72a0e6f421
Ed Overton <e.overton@gmail.com>
2018-04-01 21:58:30 -0400

    merge

--- README.md
+++ README.md
@@ -49,7 +49,7 @@ depends 'prbsrv'
 Then, in a recipe:
 
 ```
-include_recipe 'prbsrv::fix_netcool'
+include_recipe 'prbsrv::make_ncp81'
 ```
 
 If your cookbook provides resources, be sure to include examples of how to use
--- recipes/make_ncp81.rb
+++ recipes/make_ncp81.rb
@@ -1,16 +1,4 @@
-# add/create any file systems that are needed for netcool
-include_recipe 'objsrv::filesystem'
-# fix anything with RHEL that is needed for netcool
-include_recipe 'objsrv::fix_netcool'
-# install the latest version of installation manager
-include_recipe 'objsrv::install_im'
-# install base netcool
-include_recipe 'objsrv::install_nco81'
-# remove when you do not need any X11 stuff
-include_recipe 'objsrv::add_x11'
-# Update Netcool to latest fix pack
-include_recipe 'objsrv::update_nco81'
-# install the netcool knowledge library
-include_recipe 'objsrv::install_nckl'
+# install the netcool base software
+include_recipe 'objsrv::make_nc_base'
 # install the netcool probes
 include_recipe '::install_ncp81'
