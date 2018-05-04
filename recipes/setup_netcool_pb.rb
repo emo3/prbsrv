12d9c59ed5779fe38dde99e7218524d695021e66
Ed Overton <e.overton@gmail.com>
2018-05-04 16:23:30 -0400

    fix objsrv copies

--- recipes/setup_netcool_pb.rb
+++ recipes/setup_netcool_pb.rb
@@ -6,146 +6,42 @@ hostsfile_entry node['PAP'] do
 end
 
 # create omni.dat via template
-template "#{node['objsrv']['nc_dir']}/etc/omni.dat" do
+template "#{node['prbsrv']['nc_dir']}/etc/omni.dat" do
   source 'omni.dat.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
+  user node['prbsrv']['nc_act']
+  group node['prbsrv']['nc_grp']
   mode 0664
 end
 
 # create the interface file
 execute 'create_interface' do
-  command "#{node['objsrv']['nc_dir']}/bin/nco_igen"
-  cwd "#{node['objsrv']['nc_dir']}/bin"
-  not_if { File.exist?("#{node['objsrv']['nc_dir']}/etc/interfaces.linux2x86") }
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
-  action :run
-end
-
-# create the object server database
-execute 'create_objsrv' do
-  command "#{node['objsrv']['ob_dir']}/bin/nco_dbinit \
-  -server #{node['objsrv']['ncoms']} \
-  -force"
-  cwd "#{node['objsrv']['ob_dir']}/bin"
-  not_if { File.exist?("#{node['objsrv']['ob_dir']}/db/#{node['objsrv']['ncoms']}/master_store.tab") }
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
+  command "#{node['prbsrv']['nc_dir']}/bin/nco_igen"
+  cwd "#{node['prbsrv']['nc_dir']}/bin"
+  not_if { File.exist?("#{node['prbsrv']['nc_dir']}/etc/interfaces.linux2x86") }
+  user node['prbsrv']['nc_act']
+  group node['prbsrv']['nc_grp']
   action :run
 end
 
 # create ncoms property file
-template "#{node['objsrv']['ob_dir']}/etc/#{node['objsrv']['ncoms']}.props" do
+template "#{node['prbsrv']['ob_dir']}/etc/#{node['prbsrv']['ncoms']}.props" do
   source 'ncoms.props.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
+  user node['prbsrv']['nc_act']
+  group node['prbsrv']['nc_grp']
   mode 0444
-end
-
-# start the object server
-bash 'run_objsrv' do
-  cwd "#{node['objsrv']['ob_dir']}/bin"
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
-  code <<-EOH
-    #{node['objsrv']['ob_dir']}/bin/nco_objserv \
-    -name #{node['objsrv']['ncoms']} &
-    # Give time for the Object Server to come up completely
-    sleep 5
-  EOH
-  not_if { File.exist?("#{node['objsrv']['ob_dir']}/var/#{node['objsrv']['ncoms']}.pid") }
-  action :run
-end
-
-# create sql file to add user and password
-template "#{node['objsrv']['temp_dir']}/create_user.sql" do
-  source 'create_user.sql.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
-  sensitive true
-  mode 0440
-end
-
-# Create user netcool within Object Server
-execute 'create_netcool' do
-  command "#{node['objsrv']['ob_dir']}/bin/nco_sql \
-  -server #{node['objsrv']['ncoms']} \
-  -user root \
-  -password '' \
-  -input #{node['objsrv']['temp_dir']}/create_user.sql"
-  sensitive true
-  action :run
-end
-
-file "#{node['objsrv']['temp_dir']}/create_user.sql" do
-  action :delete
-end
-
-# create sql file to change root password
-template "#{node['objsrv']['temp_dir']}/set_rpwd.sql" do
-  source 'set_rpwd.sql.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
-  sensitive true
-  mode 0440
-end
-
-# Change root password
-execute 'change_root' do
-  command "#{node['objsrv']['ob_dir']}/bin/nco_sql \
-  -server #{node['objsrv']['ncoms']} \
-  -user root \
-  -password '' \
-  -input #{node['objsrv']['temp_dir']}/set_rpwd.sql"
-  sensitive true
-  action :run
-end
-
-file "#{node['objsrv']['temp_dir']}/set_rpwd.sql" do
-  action :delete
-end
-
-# This will stop the object server via nco_sql
-# create sql file to do shutdown
-template "#{node['objsrv']['temp_dir']}/shutdown.sql" do
-  source 'shutdown.sql.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
-  only_if { File.exist?("#{node['objsrv']['ob_dir']}/var/#{node['objsrv']['ncoms']}.pid") }
-  mode 0444
-end
-
-# shutdown object server
-execute 'shutdown_objsrv' do
-  command "#{node['objsrv']['ob_dir']}/bin/nco_sql \
-  -server #{node['objsrv']['ncoms']} \
-  -user root \
-  -password '#{node['objsrv']['root_pwd']}' \
-  -input #{node['objsrv']['temp_dir']}/shutdown.sql"
-  cwd "#{node['objsrv']['ob_dir']}/bin"
-  only_if { File.exist?("#{node['objsrv']['ob_dir']}/var/#{node['objsrv']['ncoms']}.pid") }
-  # The nco_sql will exit with error always
-  returns [0, 255]
-  sensitive true
-  action :run
-end
-
-file "#{node['objsrv']['temp_dir']}/shutdown.sql" do
-  action :delete
 end
 
 # create configuration files for PA
-template "#{node['objsrv']['ob_dir']}/etc/#{node['objsrv']['os_pa_name']}.conf" do
+template "#{node['prbsrv']['ob_dir']}/etc/#{node['prbsrv']['os_pa_name']}.conf" do
   source 'nco_pa.conf.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
+  user node['prbsrv']['nc_act']
+  group node['prbsrv']['nc_grp']
   mode 0444
 end
-template "#{node['objsrv']['ob_dir']}/etc/#{node['objsrv']['os_pa_name']}.props" do
+template "#{node['prbsrv']['ob_dir']}/etc/#{node['prbsrv']['os_pa_name']}.props" do
   source 'nco_pa.props.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
+  user node['prbsrv']['nc_act']
+  group node['prbsrv']['nc_grp']
   mode 0444
 end
 
@@ -178,9 +74,9 @@ service 'nco_pa' do
 end
 
 # create sql file to verify netcool
-template "#{node['objsrv']['ob_dir']}/verify_nc.sql" do
+template "#{node['prbsrv']['ob_dir']}/verify_nc.sql" do
   source 'verify_nc.sql.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
+  user node['prbsrv']['nc_act']
+  group node['prbsrv']['nc_grp']
   mode 0444
 end
--- recipes/setup_probes.rb
+++ recipes/setup_probes.rb
@@ -1,42 +1,42 @@
 # This will setup the gateways
-# create the Remedy Gateway dir
-directory node['objsrv']['rdy_dir'] do
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
+# create the mttrapd Gateway dir
+directory node['prbsrv']['rdy_dir'] do
+  user node['prbsrv']['nc_act']
+  group node['prbsrv']['nc_grp']
   mode '0755'
   recursive true
   action :create
 end
 
-# copy remedy template from default location
-execute 'copy_default_remedy' do
-  command "find #{node['objsrv']['ob_dir']}/gates/bmc_remedy -maxdepth 1 -type f -exec cp -t #{node['objsrv']['rdy_dir']} {} +"
+# copy mttrapd template from default location
+execute 'copy_default_mttrapd' do
+  command "find #{node['prbsrv']['ob_dir']}/probes/mttrapd -maxdepth 1 -type f -exec cp -t #{node['prbsrv']['rdy_dir']} {} +"
   cwd '/usr/bin'
-  not_if { File.exist?("#{node['objsrv']['rdy_dir']}/bmc_remedy.map") }
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
+  not_if { File.exist?("#{node['prbsrv']['rdy_dir']}/mttrapd.map") }
+  user node['prbsrv']['nc_act']
+  group node['prbsrv']['nc_grp']
   action :run
 end
 
-# create remedy configuration files
+# create mttrapd configuration files
 # map file
-template "#{node['objsrv']['rdy_dir']}/bmc_remedy.map" do
-  source 'bmc_remedy.map.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
+template "#{node['prbsrv']['rdy_dir']}/mttrapd.map" do
+  source 'mttrapd.map.erb'
+  user node['prbsrv']['nc_act']
+  group node['prbsrv']['nc_grp']
   mode 0444
 end
 # props file
-template "#{node['objsrv']['rdy_dir']}/G_BMC_REMEDY.props" do
-  source 'G_BMC_REMEDY.props.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
+template "#{node['prbsrv']['rdy_dir']}/G_BMC_mttrapd.props" do
+  source 'G_BMC_mttrapd.props.erb'
+  user node['prbsrv']['nc_act']
+  group node['prbsrv']['nc_grp']
   mode 0444
 end
 # env file
-template "#{node['objsrv']['rdy_dir']}/nco_g_bmc_remedy.env" do
-  source 'nco_g_bmc_remedy.env.erb'
-  user node['objsrv']['nc_act']
-  group node['objsrv']['nc_grp']
+template "#{node['prbsrv']['rdy_dir']}/nco_g_bmc_mttrapd.env" do
+  source 'nco_g_bmc_mttrapd.env.erb'
+  user node['prbsrv']['nc_act']
+  group node['prbsrv']['nc_grp']
   mode 0444
 end
