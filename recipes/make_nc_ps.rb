d9f9dae313780ca66e019864e9643d8a081e6895
Ed Overton <e.overton@gmail.com>
2018-09-08 02:47:12 -0400

    commit out x11

--- recipes/make_nc_ps.rb
+++ recipes/make_nc_ps.rb
@@ -2,6 +2,7 @@
 include_recipe '::nc_filesystem'
 # install the base Netcool software
 include_recipe 'nc_base::make_nc_base'
+# include_recipe 'nc_base::add_x11'
 # install the netcool probes
 include_recipe '::install_probes'
 # setup and start Object server
