885dc6f982702a6493f77b188c55e7e77d935755
Ed Overton <e.overton@gmail.com>
2018-09-18 22:34:58 -0400

    remove truck

--- Berksfile
+++ Berksfile
@@ -1,7 +1,5 @@
 source 'https://supermarket.chef.io'
 metadata
-cookbook 'delivery-truck', github: 'chef-cookbooks/delivery-truck'
-cookbook 'delivery-sugar', github: 'chef-cookbooks/delivery-sugar'
 cookbook 'nc_base', '~> 0.1.0', path: '../nc_base/'
 cookbook 'nc_tools', '~> 0.1.0', path: '../nc_tools/'
 cookbook 'server_utils', '~> 0.1.0', path: '../server_utils'
--- metadata.rb
+++ metadata.rb
@@ -5,21 +5,20 @@ license 'All Rights Reserved'
 description 'Installs/Configures prbsrv'
 long_description 'Installs/Configures prbsrv'
 version '0.1.0'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 12.1' if respond_to?(:chef_version)
 supports 'redhat'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/prbsrv/issues'
+# issues_url 'https://github.com/<insert_org_here>/prbsrv/issues' if respond_to?(:issues_url)
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/prbsrv'
-depends 'delivery-truck'
 depends 'nc_base', '~> 0.1.0'
 depends 'nc_tools', '~> 0.1.0'
 depends 'server_utils', '~> 0.1.0'
