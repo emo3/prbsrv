4dd3bd1c8f6a711688db923c9f6964bab58c40ab
Ed Overton <e.overton@gmail.com>
2019-11-02 00:22:13 -0400

    make simular to objsrv

--- metadata.rb
+++ metadata.rb
@@ -7,18 +7,11 @@ long_description 'Installs/Configures prbsrv'
 version '0.1.0'
 chef_version '>= 13.0'
 supports 'redhat'
+supports 'centos'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/prbsrv/issues' if respond_to?(:issues_url)
+issues_url 'https://github.com/<insert_org_here>/prbsrv/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/<insert_org_here>/prbsrv'
 
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/prbsrv'
-depends 'nc_base', '~> 0.1.0'
-depends 'nc_tools', '~> 0.1.0'
-depends 'server_utils', '~> 0.1.0'
+depends 'nc_base'
+depends 'nc_tools'
+depends 'server_utils'
