4b23090d2a84d6a38d08a110f7b5785f27179d7e
Ed Overton <e.overton@gmail.com>
2020-05-15 21:29:59 -0400

    update policy file

--- Policyfile.rb
+++ Policyfile.rb
@@ -13,7 +13,7 @@ default_source :supermarket
 run_list 'prbsrv::make_nc_ps'
 
 # Specify a custom source for a single cookbook:
-cookbook 'prbsrv', path: '.'
-cookbook 'nc_base', '~> 0.1', git: 'https://github.com/emo3/nc_base.git'
-cookbook 'nc_tools', '~> 0.1', git: 'https://github.com/emo3/nc_tools.git'
-cookbook 'server_utils', '~> 1.2', git: 'https://github.com/emo3/server_utils.git'
+cookbook 'prbsrv',       path: '.'
+cookbook 'nc_base',      git: 'https://github.com/emo3/nc_base.git'
+cookbook 'nc_tools',     git: 'https://github.com/emo3/nc_tools.git'
+cookbook 'server_utils', git: 'https://github.com/emo3/server_utils.git'
