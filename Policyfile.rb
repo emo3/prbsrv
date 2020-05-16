49bcad2c7f3c41a547edf4be3c63e2531e83d600
Ed Overton <e.overton@gmail.com>
2020-05-15 21:15:20 -0400

    add policy file

---
+++ Policyfile.rb
@@ -0,0 +1,19 @@
+# Policyfile.rb - Describe how you want Chef Infra Client to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://docs.chef.io/policyfile.html
+
+# A name that describes what the system you're building with Chef does.
+name 'prbsrv'
+
+# Where to find external cookbooks:
+default_source :supermarket
+
+# run_list: chef-client will run these recipes in the order specified.
+run_list 'prbsrv::make_nc_ps'
+
+# Specify a custom source for a single cookbook:
+cookbook 'prbsrv', path: '.'
+cookbook 'nc_base', '~> 0.1', git: 'https://github.com/emo3/nc_base.git'
+cookbook 'nc_tools', '~> 0.1', git: 'https://github.com/emo3/nc_tools.git'
+cookbook 'server_utils', '~> 1.2', git: 'https://github.com/emo3/server_utils.git'
