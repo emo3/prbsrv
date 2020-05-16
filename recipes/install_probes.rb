b201e2c6cb0bc9dcb358e0137c10bad463544879
Ed Overton <e.overton@gmail.com>
2020-05-16 01:24:35 -0400

    change label

--- recipes/install_probes.rb
+++ recipes/install_probes.rb
@@ -1,13 +1,13 @@
 # Install the Netcool tools defined between ()
 %w(nco-p-mttrapd nco-p-tivoli-eif nco-p-stdin).each do |tool|
-  install_tool 'do install tool' do
-    tool_package node['nc_tools']['tool_package']
-    tool_url node['nc_tools']['tool_url']
-    tool_dir node['nc_tools']['tool_dir']
-    tool_lif node['nc_tools']['tool_lif']
-    tool_version node['nc_tools']['tool_version']
-    tool_name node['nc_tools']['tool_name']
-    tool_imd node['nc_tools']['tool_imd']
+  install_tool 'do PRB install tool' do
+    tool_package node['nc_tools'][tool]['tool_package']
+    tool_url node['nc_tools'][tool]['tool_url']
+    tool_dir node['nc_tools'][tool]['tool_dir']
+    tool_lif node['nc_tools'][tool]['tool_lif']
+    tool_version node['nc_tools'][tool]['tool_version']
+    tool_name node['nc_tools'][tool]['tool_name']
+    tool_imd node['nc_tools'][tool]['tool_imd']
     action :install
   end
 end
